b554b55e92810d5761a66813067bb2187888258b
Vinegret <maks.bezruchkin33@gmail.com>
2024-02-17 20:01:16 +0300

    Restriction for Unit and Chapter models added

--- JSWebCourse.Models/Chapter.cs
+++ JSWebCourse.Models/Chapter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace JSWebCourse.Models
 {
     public class Chapter
     {
+        [Required]
         public int ChapterId { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(500)]
         public string Description { get; set; }
         public ICollection<Unit> Units { get; set; } = new List<Unit>();
     }
--- JSWebCourse.Models/Unit.cs
+++ JSWebCourse.Models/Unit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,18 @@ namespace JSWebCourse.Models
 {
     public class Unit
     {
+        [Required]
         public int UnitId {  get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Title {  get; set; }
+        [Required]
+        [MaxLength(500)]
         public string Description { get; set; }
+        [Required]
+        [MaxLength(15000)]
         public string HtmlCode {  get; set; }
+        [Required]
         public int ChapterId { get; set; }
         public Chapter? Chapter { get; set; }
     }
