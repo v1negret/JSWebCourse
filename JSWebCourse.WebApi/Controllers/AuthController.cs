f3adedcdfca48e68cbbe1bd0adf600ed23b08222
Vinegret <maks.bezruchkin33@gmail.com>
2024-02-27 21:49:10 +0300

    logout endpoint added

---
+++ JSWebCourse.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSWebCourse.WebApi.Controllers;
+
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly SignInManager<IdentityUser> _userManager;
+
+    [Authorize]
+    [Route("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await _userManager.SignOutAsync();
+        return Ok();
+    }
+}
